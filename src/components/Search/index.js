58a277c188ced1e0d8826462de3e21e0bc7c5d74
RachaelChar <charewicz.r@gmail.com>
2020-02-22 22:39:13 -0500

    add workout type select dropdown

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function Search() {
   return (
-    <div>Search Select</div>
+    <div class="form-group">
+      <label for="workoutType">Workout Type</label>
+      <select class="form-control" id="workoutType">
+        <option value="all">- Select -</option>
+        <option value="upper">Upper Body</option>
+        <option value="lower">Lower Body</option>
+        <option value="full">Full Body</option>
+        <option value="circuit">Circuit</option>
+        <option value="ladder">Ladder</option>
+      </select>
+    </div>
   );
 }
