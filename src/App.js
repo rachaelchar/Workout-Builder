0b7e53a0dbba10f93d60639ff747ca6a7fcf8af8
RachaelChar <charewicz.r@gmail.com>
2020-02-22 22:54:41 -0500

    make workout col slightly larger

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
       <Header />
       <div className="container">
         <div className="row mt-3">
-          <div className="col-8">
+          <div className="col-7">
             <Search />
             <ExerciseList />
           </div>
-          <div className="col-4">
+          <div className="col-5">
             <Workout />
           </div>
         </div>
