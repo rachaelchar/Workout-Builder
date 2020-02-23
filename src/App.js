66773d7a5562b679b194e57ef17ced879e4956aa
RachaelChar <charewicz.r@gmail.com>
2020-02-22 22:23:37 -0500

    add centered app logo in nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    
+    <Header />
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import './style.css';
+import logo from "../../workout_builder_icon.png";
 
-export default function Header(){
-  return(
-
+export default function Header() {
+  return (
+    <nav className="navbar navbar-ocean bg-yellow">
+      <span className="navbar-brand" href="#">
+        <img
+          src={logo}
+          className="img-logo"
+          width="30" height="30"
+          class="d-inline-block align-top" alt="" />
+      </span>
+    </nav>
   );
 }
