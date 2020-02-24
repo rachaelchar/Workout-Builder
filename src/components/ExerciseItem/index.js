83ee5730429fa7a8ab1b7d70b1e714ac716c04cb
RachaelChar <charewicz.r@gmail.com>
2020-02-24 11:22:14 -0500

    use ternary operator to render + or - button based on which list the item is in

--- src/components/ExerciseItem/index.js
+++ src/components/ExerciseItem/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import './style.css';
 
 export default function ExerciseItem(props) {
+
   return (
     <li className="list-group-item">{props.exercise.name}
-      <button
-        className="btn btn-ocean btn-sm float-right text-sun"
-        onClick={() => props.addToWorkout(props.exercise)}
-      >+</button>
-    </li>
+      {/* use ternary operator to render + or - button based on which list the item is in */}
+      {
+        props.list === "exerciseList" ?
+          < button
+            className="btn btn-ocean btn-sm float-right text-sun"
+            onClick={() => props.addToWorkout(props.exercise)}
+          >+</button>
+          :
+          < button
+            className="btn btn-sun btn-sm float-right text-ocean"
+            onClick={() => props.removeFromWorkout(props.exercise)
+            }
+          >-</button >
+      }
+    </li >
   );
 }
