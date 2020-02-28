a6fcf96dd3775722c2c3dd051b5685d95fa9b596
RachaelChar <charewicz.r@gmail.com>
2020-02-28 08:34:35 -0500

    lighten save button

--- src/components/SaveButton/index.js
+++ src/components/SaveButton/index.js
@@ -5,7 +5,7 @@ export default function SaveButton(props) {
   return (
     <div className="text-center">
       <button
-        className="btn btn-secondary mt-2"
+        className="btn btn-block btn-light border-secondary mt-2"
         onClick={props.saveWorkout}>
         Save
       </button>
