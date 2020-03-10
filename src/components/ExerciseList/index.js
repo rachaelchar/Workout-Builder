19fb14014392cdaf21c6bf26de2391c86c36e7e0
RachaelChar <charewicz.r@gmail.com>
2020-03-10 14:02:15 -0400

    change modal body to 'add exercise' form, fix button colors to match styling

--- src/components/ExerciseList/index.js
+++ src/components/ExerciseList/index.js
@@ -23,21 +23,58 @@ export default function ExerciseList(props) {
             removeFromWorkout={props.removeFromWorkout}
           />
         )}
-        <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
+        <Button className="btn btn-ocean" onClick={handleShow}>
+          Add Your Own Exercise
       </Button>
       </ul >
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Add Your Own Exercise</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <form>
+            <label className="pt-1">Exercise Name</label>
+            <input type="text" className="form-control"></input>
+
+            <label className="pt-3">Exercise Type</label>
+            <select className="form-control">
+              <option value="all">- Select -</option>
+              <option value="upper">Upper Body</option>
+              <option value="lower">Lower Body</option>
+              <option value="circuit">Circuit</option>
+              <option value="cardio">Cardio</option>
+            </select>
+
+            <label className="pt-3">Muscle Group</label>
+            <select className="form-control">
+              <option>- Select -</option>
+              <option value="back">Back</option>
+              <option value="biceps">Biceps</option>
+              <option value="calves">Calves</option>
+              <option value="chest">Chest</option>
+              <option value="core">Core</option>
+              <option value="glutes">Glutes</option>
+              <option value="hamstrings">Hamstrings</option>
+              <option value="quads">Quads</option>
+              <option value="shoulders">Shoulders</option>
+              <option value="triceps">Triceps</option>
+            </select>
+
+            <div className="pl-4 pt-4">
+              <input className="form-check-input" type="checkbox" value="true" id="aerobic" />
+              <label className="form-check-label" htmlFor="aerobic">
+                I would use this exercise in a circuit workout.
+            </label>
+            </div>
+
+          </form>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button className="btn-light-grey " onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button className="btn-sun" onClick={handleClose}>
             Save Changes
           </Button>
         </Modal.Footer>
