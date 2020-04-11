60b3d8688bee69287281c4c9d560920e3cbe9bbe
RachaelChar <charewicz.r@gmail.com>
2020-04-11 18:34:00 -0400

    clean up unused state hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import Home from './pages/Home';
 import MyWorkouts from './pages/MyWorkouts';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
-
-  const [savedWorkouts, setSavedWorkouts] = useState([]);
 
   return (
     <Router>
@@ -15,15 +13,11 @@ function App() {
         <Route
           exact path="/"
           component={Home}
-          savedWorkouts={savedWorkouts}
-          setSavedWorkouts={setSavedWorkouts}
         >
         </Route>
         <Route
           path="/myworkouts"
           component={MyWorkouts}
-          savedWorkouts={savedWorkouts}
-          setSavedWorkouts={setSavedWorkouts}
         ></Route>
       </div>
     </Router>
